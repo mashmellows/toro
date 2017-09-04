f12d3ffdd1c3532f86173c7aff3d77f1d0bb26ea
Xura <mashdroidd@gmail.com>
2017-09-04 18:47:14 +1000

    cascade examples

---
+++ cascades/anomaly_remover.py
@@ -0,0 +1,21 @@
+import os
+import cv2
+import numpy as np
+
+def find_uglies():
+    match = False
+    for file_type in ['neg']:
+        for img in os.listdir(file_type):
+            for ugly in os.listdir('anomaly_example'):
+                try:
+                    current_image_path = str(file_type)+'/'+str(img)
+                    ugly = cv2.imread('anomaly_example/'+str(ugly))
+                    question = cv2.imread(current_image_path)
+                    if ugly.shape == question.shape and not(np.bitwise_xor(ugly,question).any()):
+                        print('That is one ugly pic! Deleting!')
+                        print(current_image_path)
+                        os.remove(current_image_path)
+                except Exception as e:
+                    print(str(e))
+
+find_uglies()
